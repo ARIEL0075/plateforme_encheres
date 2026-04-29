3252a72dcf4408f9073a8b7b7a05a10f301abb42
Ariel HOUETO <houetoariel90@icloud.com>
2026-04-30 00:52:32 +0200

    Connexion de la plateform a la base de donnée sqlLite

--- src/database/repository.py
+++ src/database/repository.py
@@ -1,3 +1,4 @@
+import sqlite3
 from src.database.db import connect_db
 class Repository:
     
@@ -77,15 +78,15 @@ class Repository:
         finally:
             if conn:
                 conn.close()
-@staticmethod
-def get_all_objets():
-    conn = None
-    try:
-        conn = connect_db()
-        conn.row_factory = sqlite3.Row # Permet d'accéder aux colonnes par nom
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM objets")
-        return cursor.fetchall()
-    finally:
-        if conn:
+    @staticmethod
+    def get_all_objets():
+        conn = None
+        try:
+         conn = connect_db()
+         conn.row_factory = sqlite3.Row # Permet d'accéder aux colonnes par nom
+         cursor = conn.cursor()
+         cursor.execute("SELECT * FROM objets")
+         return cursor.fetchall()
+        finally:
+         if conn:
             conn.close()
